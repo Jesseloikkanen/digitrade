eea4aea28c2149de39866face6cc1b3dd53beb39
JesseLoikkanen <1701543@co.saimia.fi>
2018-01-10 15:22:55 +0200

    arvio

---
+++ itsearvio/itsearvio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Itsearvio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä sana tai lause");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i + 1}. {userInput}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ itsearvio/itsearvio3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace arvio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int summa = 0;
+            int userInput = 0;
+            bool isNumber = true;
+            Console.WriteLine("Syötä luku. Anna luku -5 saadaksesi aiempien lukujen summan");
+
+            while (isNumber)
+            {
+                Console.Write("Luku: ");
+                isNumber = int.TryParse(Console.ReadLine(), out userInput);
+
+                if (userInput == -5)
+                {
+                    break;
+                }
+                else
+                {
+                    summa += userInput;
+                }
+            }
+            Console.WriteLine($"Lukujen summa on: {summa}");
+            Console.ReadKey();
+        }
+    }
+}

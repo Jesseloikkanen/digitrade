571066d36a481377bf2855907880415c7baef2e9
JesseLoikkanen <1701543@co.saimia.fi>
2018-01-10 15:17:23 +0200

    valmis

---
+++ Conditional-statements/task6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä ohjelmaan luku: ");
+            // Define variables
+            string luku = Console.ReadLine();
+            int num = int.Parse(luku);
+            int i = 1;
+            int n = 1;
+            string parseOutput = "";
+
+            while (i <= num)
+            {
+                parseOutput = $"{parseOutput} x {i}";
+                n = n * i;
+                Console.WriteLine($"{i}!={n}"); //5!=120
+                i++;
+            }
+            if (num < 0)
+            {
+                Console.WriteLine("Määrittelemätön");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"{parseOutput}={n}");
+                Console.ReadLine();
+            }
+        }
+    }
+}

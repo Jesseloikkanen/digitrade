2fdc5c8874bf0e3b3ab53c71e6eab0caf2039de4
JesseLoikkanen <1701543@co.saimia.fi>
2018-01-10 14:26:29 +0200

    Taulukot

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lottorivi = new int[40];
+            Random rnd = new Random();
+            int i = 0;
+
+            while (i < 7)
+            {
+                int rndNumber = rnd.Next(0, 40);
+                if (lottorivi[rndNumber] == 0)
+                {
+                    lottorivi[rndNumber] = 1;
+                    i++;
+                }
+            }
+            Console.WriteLine("Oikea lottorivi on: ");
+            for (i = 0; i < lottorivi.Length; i++)
+            {
+                if (lottorivi[i] == 1)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            while (true)
+            {
+                int rndNumber = rnd.Next(0, 40);
+                if (lottorivi[rndNumber] == 0)
+                {
+                    lottorivi[rndNumber] = 2;
+                    break;
+                }
+            }
+            for (i = 0; i < lottorivi.Length; i++)
+            {
+                if (lottorivi[i] == 2)
+                {
+                    Console.WriteLine($"\nLisänumero: {i}");
+                }
+            }
+            Console.Write($"Tuplausnumero: {rnd.Next(0, 40)}");
+            Console.ReadKey();
+        }
+    }
+}
